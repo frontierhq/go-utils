91f3f40ccedf5d1b3a6f8f2cf75951ebe6c8fb25
Fraser Davidson <fraser@frontierdigital.net>
2023-08-04 11:41:04 +0100

    WIP

--- git/external_git/external_git.go
+++ git/external_git/external_git.go
@@ -1,7 +1,14 @@
 package external_git
 
+import "path/filepath"
+
 type ExternalGit struct {
 	repositoryPath string
+}
+
+// GetFilePath returns the absolute path to a file in the repository
+func (g *ExternalGit) GetFilePath(filePath string) string {
+	return filepath.Join(g.repositoryPath, filePath)
 }
 
 // GetRepositoryPath returns the absolute path to the repository
