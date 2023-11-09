2cf266df76b96caf81dfd6e53573eed68d7d5cda
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2023-11-09 10:31:54 +0000

    FTR-101 Choose the version type for the file content. (#9)
    * FTR-101 Choose the version type for the file content.

--- azuredevops/git.go
+++ azuredevops/git.go
@@ -1,19 +1,33 @@
 package azuredevops
 
 import (
+	"fmt"
+
 	"github.com/microsoft/azure-devops-go-api/azuredevops/git"
 )
 
 // GetFileContent gets content in a file over API.
-func (a *AzureDevOps) GetFileContent(projectName string, repoName string, version string, filepath string) (*git.GitItem, error) {
+func (a *AzureDevOps) GetFileContent(projectName string, repoName string, version string, filepath string, versionType string) (*git.GitItem, error) {
 	client, err := git.NewClient(a.ctx, a.connection)
 	if err != nil {
 		return nil, err
 	}
 
+	var vt git.GitVersionType
+	switch versionType {
+	case "branch":
+		vt = git.GitVersionTypeValues.Branch
+	case "commit":
+		vt = git.GitVersionTypeValues.Commit
+	case "tag":
+		vt = git.GitVersionTypeValues.Tag
+	default:
+		return nil, fmt.Errorf("unknown version type: %s", versionType)
+	}
+
 	includeContent := true
 	gitVersionDescriptor := git.GitVersionDescriptor{
-		VersionType: &git.GitVersionTypeValues.Tag,
+		VersionType: &vt,
 		Version:     &version,
 	}
 	getItemArgs := git.GetItemArgs{
